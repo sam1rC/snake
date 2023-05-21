5378c68982b8dad0578eb641bc83ac6d74f30b62
Samir Colpas <samir.colpas@hotmail.com>
2023-05-21 14:43:50 -0500

    fix snake movement

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -12,25 +12,78 @@ class HomePage extends StatefulWidget {
   State<HomePage> createState() => _HomePageState();
 }
 
+enum snake_Direction { UP, DOWN, LEFT, RIGHT }
+
 class _HomePageState extends State<HomePage> {
   //grid dimensions
   int rowSize = 10;
   int totalNumberOfSquares = 100;
   //snake position
   List<int> snakePos = [0, 1, 2];
+  //snake direction is initially to the right
+  var currentDirection = snake_Direction.RIGHT;
   //food position
   int foodPos = 55;
   //start game
   void startGame() {
     Timer.periodic(Duration(milliseconds: 200), (timer) {
-      setState(() {
-        //add a new head
-        snakePos.add(snakePos.last + 1);
-
-        //remove the tail
-        snakePos.remove(0);
-      });
+      setState(() {});
     });
+  }
+
+  void moveSnake() {
+    switch (currentDirection) {
+      case snake_Direction.RIGHT:
+        {
+          //add a head
+          //if snake is at the right wall
+          if (snakePos.last % rowSize == 9) {
+            snakePos.add(snakePos.last + 1 - rowSize);
+          } else {
+            snakePos.add(snakePos.last + 1);
+          }
+
+          //remove tail
+          snakePos.removeAt(0);
+        }
+      case snake_Direction.LEFT:
+        {
+          //add a head
+          //if snake is at the right wall
+          if (snakePos.last % rowSize == 0) {
+            snakePos.add(snakePos.last - 1 + rowSize);
+          } else {
+            snakePos.add(snakePos.last - 1);
+          }
+          //remove tail
+          snakePos.removeAt(0);
+        }
+      case snake_Direction.UP:
+        {
+          //add a head
+          if (snakePos.last < rowSize) {
+            snakePos.add(snakePos.last - rowSize + totalNumberOfSquares);
+          } else {
+            snakePos.add(snakePos.last - rowSize);
+          }
+          //remove tail
+          snakePos.removeAt(0);
+        }
+      case snake_Direction.DOWN:
+        {
+          //add a head
+          if (snakePos.last + rowSize > totalNumberOfSquares) {
+            snakePos.add(snakePos.last + rowSize - totalNumberOfSquares);
+          } else {
+            snakePos.add(snakePos.last + rowSize);
+          }
+          //remove tail
+          snakePos.removeAt(0);
+        }
+
+        break;
+      default:
+    }
   }
 
   @override
@@ -46,32 +99,50 @@ class _HomePageState extends State<HomePage> {
           //game grid
           Expanded(
             flex: 3,
-            child: GridView.builder(
-              itemCount: totalNumberOfSquares,
-              physics: const NeverScrollableScrollPhysics(),
-              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: rowSize,
-              ),
-              itemBuilder: (BuildContext context, int index) {
-                if (snakePos.contains(index)) {
-                  return const SnakePixel();
-                } else if (foodPos == index) {
-                  return const FoodPixel();
-                } else {
-                  return const BlankPixel();
+            child: GestureDetector(
+              onVerticalDragUpdate: (details) {
+                if (details.delta.dy > 0 &&
+                    currentDirection != snake_Direction.UP) {
+                  currentDirection = snake_Direction.DOWN;
+                } else if (details.delta.dy < 0 &&
+                    currentDirection != snake_Direction.DOWN) {
+                  currentDirection = snake_Direction.UP;
                 }
               },
+              onHorizontalDragUpdate: (details) {
+                if (details.delta.dx > 0 &&
+                    currentDirection != snake_Direction.LEFT) {
+                  currentDirection = snake_Direction.RIGHT;
+                } else if (details.delta.dx < 0 &&
+                    currentDirection != snake_Direction.RIGHT) {
+                  currentDirection = snake_Direction.LEFT;
+                }
+              },
+              child: GridView.builder(
+                itemCount: totalNumberOfSquares,
+                physics: const NeverScrollableScrollPhysics(),
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: rowSize,
+                ),
+                itemBuilder: (BuildContext context, int index) {
+                  if (snakePos.contains(index)) {
+                    return const SnakePixel();
+                  } else if (foodPos == index) {
+                    return const FoodPixel();
+                  } else {
+                    return const BlankPixel();
+                  }
+                },
+              ),
             ),
           ),
           //playbtn
           Expanded(
-            child: Container(
-              child: Center(
-                child: MaterialButton(
-                  onPressed: startGame,
-                  child: Text("Jugar"),
-                  color: Colors.pink,
-                ),
+            child: Center(
+              child: MaterialButton(
+                onPressed: startGame,
+                color: Colors.pink,
+                child: const Text("Jugar"),
               ),
             ),
           ),
