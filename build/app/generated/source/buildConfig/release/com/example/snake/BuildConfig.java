2e167998690030e243d59ed4468444365baa1d94
andres <andresc.gomez@upb.edu.co>
2023-05-23 19:10:39 -0500

    uodate release for android

--- build/app/generated/source/buildConfig/release/com/example/snake/BuildConfig.java
+++ build/app/generated/source/buildConfig/release/com/example/snake/BuildConfig.java
@@ -5,7 +5,7 @@ package com.example.snake;
 
 public final class BuildConfig {
   public static final boolean DEBUG = false;
-  public static final String APPLICATION_ID = "com.example.snake";
+  public static final String APPLICATION_ID = "co.edu.upb.snake";
   public static final String BUILD_TYPE = "release";
   public static final int VERSION_CODE = 1;
   public static final String VERSION_NAME = "1.0.0";
